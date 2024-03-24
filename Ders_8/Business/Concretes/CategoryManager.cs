5839c490db71f6b6eda1edd8dc00784a9aebb229
Gönül Kaba <gonul.kaba123@gmail.com>
2024-03-25 01:37:34 +0300

    Ders 8 uygulama

---
+++ Ders_8/Business/Concretes/CategoryManager.cs
@@ -0,0 +1,53 @@
+﻿using Business.Abstracts;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes
+{
+    public class CategoryManager : ICategoryService
+    {
+        List<Category> _categories;
+
+        public CategoryManager()
+        {
+            _categories = new List<Category>();
+        }
+        public void Add(Category category)
+        {
+            _categories.Add(category);
+        }
+
+        public void Delete(int id)
+        {
+            Category category = _categories.FirstOrDefault(x => x.Id == id);
+            if (category != null)
+            {
+                _categories.Remove(category);
+            }
+        }
+
+        public List<Category> GetAll()
+        {
+            return this._categories;
+        }
+
+        public Category GetById(int id)
+        {
+            return _categories.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Update(Category updatedCategory)
+        {
+            Category category = _categories.FirstOrDefault(x => x.Id == updatedCategory.Id);
+            if (category != null)
+            {
+                category.Name = updatedCategory.Name;
+                category.Description = updatedCategory.Description;
+            }
+        }
+    }
+}
---
+++ Ders_8/Business/Concretes/ProductManager.cs
@@ -0,0 +1,55 @@
+﻿using Business.Abstracts;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes
+{
+    public class ProductManager : IProductService
+    {
+        List<Product> _products;
+
+        public ProductManager()
+        {
+            this._products = new List<Product>();
+        }
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(int id)
+        {
+            Product product = _products.FirstOrDefault(x => x.Id == id);
+            if (product != null)
+            {
+                _products.Remove(product);
+            }
+        }
+
+        public List<Product> GetAll()
+        {
+            return this._products;
+        }
+
+        public Product GetById(int id)
+        {
+            return _products.FirstOrDefault(x => x.Id == id);
+
+        }
+
+        public void Update(Product updatedProduct)
+        {
+            Product product = _products.FirstOrDefault(x => x.Id == updatedProduct.Id);
+            if (product != null)
+            {
+                product.Name = updatedProduct.Name;
+                product.UnitPrice = updatedProduct.UnitPrice;
+            }
+        }
+    }
+}
