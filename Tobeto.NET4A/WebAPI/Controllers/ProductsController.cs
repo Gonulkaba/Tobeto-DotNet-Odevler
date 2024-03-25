5737d1ca16664243283a6a6af20ebdbb70fccebd
Gönül Kaba <gonul.kaba123@gmail.com>
2024-03-25 16:47:42 +0300

    Ders-8 katmanlı mimariye giriş. InMemory

---
+++ Tobeto.NET4A/WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,49 @@
+﻿using Business.Abstracts;
+using Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        IProductService productService;
+
+        public ProductsController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> GetAll()
+        {
+            return productService.GetAll();
+        }
+
+        [HttpPost]
+        public void Add([FromBody] Product product)
+        {
+            productService.Add(product);
+        }
+
+        [HttpGet("id")]
+        public Product GetById([FromQuery] int id)
+        {
+            return productService.GetById(id);
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            productService.Delete(id);
+        }
+
+        [HttpPut]
+        public void Update(Product product)
+        {
+            productService.Update(product);
+        }
+    }
+}
