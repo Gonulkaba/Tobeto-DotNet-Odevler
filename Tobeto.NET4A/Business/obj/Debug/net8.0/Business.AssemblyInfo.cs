93003176bbc3cad798138d0af47af4426986d8a0
Gönül Kaba <gonul.kaba123@gmail.com>
2024-04-17 13:12:16 +0300

    Ders-15 Fluent Validation & MediatR pipelines işlemleri

--- Tobeto.NET4A/Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
+++ Tobeto.NET4A/Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14db698c2de7369a96adc39d076e0c68c1843711")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a5e46832958341959f76be61db50c3ea5dce41c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Tobeto.NET4A/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Tobeto.NET4A/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14db698c2de7369a96adc39d076e0c68c1843711")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a5e46832958341959f76be61db50c3ea5dce41c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
