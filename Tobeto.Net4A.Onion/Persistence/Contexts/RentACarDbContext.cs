b6d67e41c1172dc474915d105186c3e1a797d9e0
Gönül Kaba <gonul.kaba123@gmail.com>
2024-04-24 20:56:44 +0300

    Ders-19 Onion mimari proje oluşturuldu

---
+++ Tobeto.Net4A.Onion/Persistence/Contexts/RentACarDbContext.cs
@@ -0,0 +1,20 @@
+﻿﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Contexts
+{
+    public class RentACarDbContext : DbContext
+    {
+        public DbSet<Brand> Brands { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-8SGA7NI\\SQLEXPRESS;Initial Catalog=Tobeto4A2.Onion;Integrated Security=True; Trust Server Certificate=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
